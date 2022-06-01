f404d15a53edffd31a818d6032c57fd63d7f5027
paraskeuos <57093460+paraskeuos@users.noreply.github.com>
2022-06-01 13:40:10 +0200

    Add Adjacency tree code to utils.py

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.patches import Polygon
 import networkx as nx
+import gudhi
 
 
 def load_data(county='025-imperial'):
@@ -123,3 +124,99 @@ def plot_loops(gdf, points, persistance_1D_with_loops):
 #         color = 'darkgreen' if long_lived else 'lightgreen'
 
         ax.plot(x, y, color=color, linewidth=linewidth)
+    
+def get_adjacency_simplex_tree(gdf, pct_id_to_vert):
+    delta = 0.95
+    simplex_tree = gudhi.SimplexTree()
+
+    edges = []
+    triangles = []
+
+    while delta >= 0.5:
+        curr_complex = []
+        vertices = gdf[gdf['delta'] >= delta].index.tolist()
+    
+        # Add 0-simplices
+        for v in vertices:
+            # Don't add duplicates from a complex with higher delta
+            vert_index = pct_id_to_vert[v]
+            duplicate = False
+            for simplex, fval in simplex_tree.get_filtration():
+                if len(simplex) != 1:
+                    continue
+                if simplex[0] == vert_index:
+                    duplicate = True
+                    break
+            if not duplicate:
+                curr_complex.append([vert_index])
+    
+        # Find neighbors for each vertex
+        neighbors_dict = {}
+        tmp = gdf.loc[vertices, :]
+        for index, row in tmp.iterrows():
+            neighbors_dict[index] = tmp[tmp['geometry'].touches(row['geometry'])].index.tolist()
+    
+        # Add 1-simplices
+        for key, neighbors in neighbors_dict.items():
+            for val in neighbors:
+                simplex = [pct_id_to_vert[val], pct_id_to_vert[key]]
+                simplex.sort()
+                # Don't add duplicate 1-simplices from the same complex
+                if simplex in curr_complex:
+                    continue
+            
+                # Don't add duplicates from a complex with higher delta
+                duplicate = False
+                for added_simplex, fval in simplex_tree.get_filtration():
+                    if len(added_simplex) != 2:
+                        continue
+                    if np.array_equal(simplex, added_simplex):
+                        duplicate = True
+                        break
+                if not duplicate:
+                    curr_complex.append(simplex)
+                    edges.append(simplex)
+
+        for simplex in curr_complex:
+            simplex_tree.insert(simplex, 1-delta)
+        
+        # Add 2-simplices
+        edges.sort()
+        for i in range(len(edges)):
+            v1 = edges[i][0]
+            v2 = edges[i][1]
+            for j in range(i+1, len(edges)):
+                foundTriangle = False
+                if v1 in edges[j] or v2 in edges[j]:
+                    # Found edge2 which has v1 or v2
+                    edge3 = []
+                
+                    if v1 in edges[j]:
+                        edge3.append(v2)
+                        edge3.append(edges[j][1] if v1 == edges[j][0] else edges[j][0])
+                    else:
+                        edge3.append(v1)
+                        edge3.append(edges[j][1] if v2 == edges[j][0] else edges[j][0])
+                
+                    edge3.sort()
+                    for k in range(j+1, len(edges)):
+                        if np.array_equal(edge3, edges[k]):
+                            triangle = [v1, v2]
+                            for v in edges[k]:
+                                if v not in triangle:
+                                    triangle.append(v)
+                                    break
+                       
+                            triangle.sort()
+                            foundTriangle = True
+                            if triangle not in triangles:
+                                triangles.append(triangle)
+                                simplex_tree.insert(triangle, 1-delta)
+                    
+                            break
+                    if foundTriangle:
+                        break
+   
+        delta = round(delta-0.05, 2)
+    
+    return simplex_tree
